9e0518b7655d1412d94a0b569842300158e7e2af
Soichi Hayashi <hayashis@iu.edu>
2018-08-30 11:17:58 -0400

    added script to split up generated tract masks

--- run.sh
+++ run.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-#cuda/nvidia drivers comes from the host. it needs to be mounted by singularity
-#export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=/usr/lib/nvidia-390:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=/usr/lib/nvidia-390/extra:$LD_LIBRARY_PATH
+set -e
 
 source /etc/fsl/5.0/fsl.sh #enable fsl
 export PATH=$PATH:/usr/lib/mrtrix/bin #enable mrtrix
 export HOME=/ #so that tractseg uses /.tractseg not ~/.tractseg
 
-#echo "using models"
-#ls -la ~/.tractseg
-
 TractSeg -i $(jq -r .dwi config.json) -o .
+
+#split mask into individual files to be consistent with neuro/mask/tracts datatype
+rm -rf masks
+mkdir -p masks
+(
+	cd masks
+	fslsplit ../tractseg_output/bundle_segmentations.nii.gz
+
+	#load tractnames.txt (massage some characters) - from TractSeg README
+	names=($(cat ../tractnames.txt | tr -d '() '))
+	
+	#rename each volume to the tractseg names (postfix by _Vol.nii.gz)
+	id=0
+	for file in $(ls vol*.nii.gz | sort); do
+		mv $file ${names[$id]}_Vol.nii.gz
+		let "id = id + 1"
+	done
+
+	#TODO - what about colors.json?
+)
+
