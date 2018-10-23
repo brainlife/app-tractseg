671fc90fa6c36d646ea2235c6aca29caa954348c
Soichi Hayashi <hayashis@iu.edu>
2018-10-23 17:24:55 -0400

    update create_fgclassified to grab first 500 per tracts for tracts/*.json

--- create_fgclassified.py
+++ create_fgclassified.py
@@ -20,7 +20,7 @@ encoder.FLOAT_REPR = lambda o: format(o, '.3f') #.3 too aggresive?
 fg_classified = np.zeros([72], dtype={'names':('name', 'fibers'), 'formats':('U14', 'object')})
 name = []
 fibers = []
-fiber_count = []
+fiber_counts = []
 norm = matplotlib.colors.Normalize(vmin=1, vmax=72)
 
 tractsfile = []
@@ -32,17 +32,21 @@ for file in glob.glob("tractseg_output/TOM_trackings" + "/*.tck"):
     tractname = os.path.basename(file).split('.tck')[0]  
     name.append(np.array(tractname))
     
-    fiber_count.append(len(tck.streamlines))
-    streamlines = np.zeros([len(tck.streamlines)], dtype=object)
-    for e in range(len(tck.streamlines)):
+    count = len(tck.streamlines)
+    fiber_counts.append(count)
+
+    streamlines = np.zeros([count], dtype=object)
+    for e in range(count):
         streamlines[e] = np.transpose(tck.streamlines[e])
-    fibers.append(np.reshape(streamlines, [len(tck.streamlines),1]))
-    
-    jsonfibers = np.reshape(streamlines, [len(tck.streamlines),1]).tolist()
-    for i in range(len(jsonfibers)):
+    fibers.append(np.reshape(streamlines, [count,1]))
+
+    #grab first 500 fibers
+    max=500
+    jsonfibers = np.reshape(streamlines[:max], [max,1]).tolist()
+    for i in range(max):
         jsonfibers[i] = [jsonfibers[i][0].tolist()]
+
     jsonfile = {'name': tractname, 'color': list(cm.nipy_spectral(norm(n)))[0:3], 'coords': jsonfibers}
-    #list(colors[n])
     
     splitname = tractname.split('_')
     fullname = splitname[-1].capitalize()+' '+' '.join(splitname[0:-1])  
@@ -51,6 +55,7 @@ for file in glob.glob("tractseg_output/TOM_trackings" + "/*.tck"):
     
     with open ('tracts/'+str(n)+'.json', 'w') as outfile:
         #json.dump(jsonfile, outfile, separators=(',', ': '), indent=4)
+        print("  writing json")
         json.dump(jsonfile, outfile)
     n+=1
 
@@ -62,8 +67,8 @@ fg_classified['fibers'] = fibers
 
 txtfile = open("output_fibercounts.txt","w") 
 txtfile.write("Tracts,FiberCount\n") 
-for i in range(len(fiber_count)):
-    txtfile.write(str(name[i])+','+str(fiber_count[i])+'\n') 
+for i in range(len(fiber_counts)):
+    txtfile.write(str(name[i])+','+str(fiber_counts[i])+'\n') 
 txtfile.close()
 
 sio.savemat('output.mat', {'fg_classified': fg_classified})
