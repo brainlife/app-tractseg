56860f20e72a1026f665ec4918c94859d9494a92
Giulia Berto <gberto@fbk.eu>
2022-09-13 12:08:38 -0500

    restore preprocessing option and add  optional T1

--- run.sh
+++ run.sh
@@ -10,19 +10,27 @@ source /etc/fsl/5.0/fsl.sh #enable fsl (fsl6 still uses /etc/fsl/5.0.. for some 
 export PATH=$PATH:/usr/lib/mrtrix/bin #enable mrtrix
 export HOME=/ #so that tractseg uses /.tractseg not ~/.tractseg to look for prestaged models
 
-#opts=""
-#if [ $(jq -r .preprocess config.json) == "true" ]; then
-#	opts="$opts --preprocess"
-#fi
+opts=""
+if [ $(jq -r .preprocess config.json) == "true" ]; then
+	opts="$opts --preprocess"
+fi
 
 ln -sf $(jq -r .dwi config.json) dwi.nii.gz
 ln -sf $(jq -r .bvecs config.json) dwi.bvecs
 ln -sf $(jq -r .bvals config.json) dwi.bvals
 
-#t1=`jq -r '.t1' config.json`
-#if [ $t1 != "null" ]; then
-#	ln -sf $t1 T1w_acpc_dc_restore_brain.nii.gz
-#fi
+t1=`jq -r '.t1' config.json`
+if [ $t1 != "null" ]; then
+	ln -sf $t1 T1w_acpc_dc_restore_brain.nii.gz
+fi
+
+csd_type=`jq -r '.csd' config.json`
+if [ $csd_type == "csd_msmt_5tt" ]; then
+    if [ $t1 == "null" ]; then
+	    echo "Error: csd_msmt_5tt requires a T1w image."
+	    exit 1
+    fi
+fi
 
 echo "(1/4) running tract_segmentation"
 TractSeg -i dwi.nii.gz --raw_diffusion_input \
@@ -30,14 +38,16 @@ TractSeg -i dwi.nii.gz --raw_diffusion_input \
     --output_type tract_segmentation \
     --keep_intermediate_files \
     --nr_cpus 8 \
-    -o tractseg_output
+    -o tractseg_output \
+	$opts
 
 ##Get segmentations of the regions were the bundles start and end (helpful for filtering fibers that do not run from start until end).
 echo "(2/4) running endings_segmentation"
 TractSeg -i tractseg_output/peaks.nii.gz \
     --output_type endings_segmentation \
     --nr_cpus 8 \
-    -o tractseg_output
+    -o tractseg_output \
+	$opts
 
 #For each bundle create a Tract Orientation Map (Wasserthal et al., Tract orientation mapping for bundle-specific tractography). 
 #This gives you one peak per voxel telling you the main orientation of the respective bundle at this voxel. Can be used for 
@@ -48,7 +58,8 @@ echo "(3/4) running TOM/tracking"
 TractSeg -i tractseg_output/peaks.nii.gz \
     --output_type TOM \
     --nr_cpus 8 \
-    -o tractseg_output
+    -o tractseg_output \
+	$opts
 
 Tracking -i tractseg_output/peaks.nii.gz \
     --tracking_format tck \
